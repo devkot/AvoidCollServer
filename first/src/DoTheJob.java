9beb046bba0c22036ef4551dfe02656bc9e72d1b
Iakovos <sdi1300191@di.uoa.gr>
2017-01-11 18:41:35 +0200

    all fixed with cases

--- first/src/DoTheJob.java
+++ first/src/DoTheJob.java
@@ -8,17 +8,17 @@ public class DoTheJob {
     public static float pvaluex = 0;
     public static float pvaluey = 0;
     public static float pvaluez = 0;
-    public static String pid=null;
-    public static String id = null;
+    public static String pid="null";
+    public static String id = "null";
     public static boolean collision = false;
-    public static boolean dcol = false;
     public static long timef = 0;
-    public static float pspeed = 0;
+    //public static float pspeed = 0;
     public DoTheJob(String top, String mes,String time) {
         float valuex=0;
         float valuey=0;
         float valuez=0;
         float speed;
+        boolean dcol = false;
         int i=0,j=0;
         List<String> strM;
         strM = new ArrayList<>();
@@ -26,8 +26,7 @@ public class DoTheJob {
         List<String> str;
         str = new ArrayList<>();
         str.add(0, top);
-        String Ltopic = new String();
-        pid=id;
+        //String Ltopic = new String();
         for (String RR : str.get(0).split("/")) {
             if(i==1) {
                 switch (RR) {
@@ -36,24 +35,26 @@ public class DoTheJob {
                         if(!(id.equals(pid))){
                             dcol=true;
                             System.out.println("different ids true");
-                            //timef=System.currentTimeMillis();
-                            //System.out.println("Timef ="+timef);
                         }
                         System.out.println("listening to light sensor");
                         for(String RRM : strM.get(0).split("/")){
                             float x=Float.parseFloat(RRM);
-                            if(x<40.0){
-                                collision=true;
-
-                                Publisher.main("Light/Danger","Danger!!! Low Light!!!");
-                                System.out.println("Timef ="+timef);
-                            }
                             if(dcol && collision){
                                 System.out.println("Timef - Current="+(System.currentTimeMillis()-timef));
                                 if ((System.currentTimeMillis()-timef)<1000) {
 
-                                    Publisher.main("Proximity/Confirmed","Collision Confirmed"+time);
+                                    Publisher.main("Light/Confirmed","Collision Confirmed "+time);
+                                    System.out.println("Between "+id+" and "+pid);
                                 }
+
+                            }
+                            if(x<40.0){
+                                collision=true;
+                                timef=System.currentTimeMillis();
+                                Publisher.main("Light/Danger","Danger!!! Low Light!!! "+time);
+                                System.out.println("Timef ="+timef);
+                            }else{
+                                collision=false;
 
                             }
                         }
@@ -62,25 +63,27 @@ public class DoTheJob {
                         if(!(id.equals(pid))){
                             dcol=true;
                             System.out.println("different ids true");
-                            //timef=System.currentTimeMillis();
-                            //System.out.println("Timef ="+timef);
                         }
                         System.out.println("listening to proximity sensor");
                         for(String RRM : strM.get(0).split("/")){
                             float y=Float.parseFloat(RRM);
-                            if(y==0.0){
-                                collision=true;
-                                //timef=System.currentTimeMillis();
-                                Publisher.main("Proximity/Danger","Danger!!! To close to Object");
-                            }
-                            System.out.println("dcol "+dcol+"collision"+collision);
+                            System.out.println("dcol "+dcol+" collision "+collision);
                             if(dcol && collision){
                                 System.out.println("timef="+timef+" Current="+System.currentTimeMillis());
                                 System.out.println("Timef - Current="+(System.currentTimeMillis()-timef));
                                 if ((System.currentTimeMillis()-timef)<1000) {
 
-                                    Publisher.main("Proximity/Confirmed","Collision Confirmed"+time);
+                                    Publisher.main("Proximity/Confirmed","Collision Confirmed "+time);
+                                    System.out.println("Between "+id+" and "+pid);
                                 }
+
+                            }
+                            if(y==0.0){
+                                collision=true;
+                                timef=System.currentTimeMillis();
+                                Publisher.main("Proximity/Danger","Danger!!! To close to Object "+time);
+                            }else{
+                                collision=false;
 
                             }
                         }
@@ -89,11 +92,8 @@ public class DoTheJob {
                         if(!(id.equals(pid))){
                             dcol=true;
                             System.out.println("different ids true");
-                            //timef=System.currentTimeMillis();
-                            //System.out.println("Timef ="+timef);
                         }
                         System.out.println("listening to Acceleration sensor");
-                        Ltopic = RR;
                         int k=0;
                         for(String RRM : strM.get(0).split("/")){
                             if(k==0) {
@@ -114,19 +114,22 @@ public class DoTheJob {
                         pvaluey=valuey;
                         pvaluez=valuez;
                         //speedcase if
-                        if(speed>2.0){
-                            collision=true;
-                            timef=System.currentTimeMillis();
-                            Publisher.main("Acceleration/Danger","Danger!!! Moving too Fast");
-
-                        }
                         if(dcol && collision){
                             System.out.println("Timef - Current="+(System.currentTimeMillis()-timef));
                             if ((System.currentTimeMillis()-timef)<1000) {
 
-                                Publisher.main("Acceleration/Confirmed","Collision Confirmed"+time);
+                                Publisher.main("Acceleration/Confirmed","Collision Confirmed "+time);
+                                System.out.println("Between "+id+" and "+pid);
                             }
 
+                        }
+                        if(speed>2.0){
+                            collision=true;
+                            timef=System.currentTimeMillis();
+                            Publisher.main("Acceleration/Danger","Danger!!! Moving too Fast "+time);
+
+                        }else{
+                            collision=false;
                         }
                         break;
                     default:
@@ -137,19 +140,11 @@ public class DoTheJob {
             if(i==0) {
                 if (!(RR.equals("Proximity") || RR.equals("Light") || RR.equals("Acceleration"))) {
                     id = RR;
-                    //System.out.println("now " + id + " previous " + pid);
                 }
 
             }
             i++;
         }
-        if(!(id.equals(pid))){
-            dcol=true;
-            System.out.println("different ids true");
-            timef=System.currentTimeMillis();
-            System.out.println("Timef ="+timef);
-        }else{
-            dcol=false;
-        }
+        pid=id;
     }
 }
