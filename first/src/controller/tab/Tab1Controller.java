db23912391f77d7cfbeb97dc50f627823cc861f8
Agamemnon Apladas <sdi1300013@di.uoa.gr>
2017-01-14 18:48:05 +0200

    setting the java fx and mysql queries in tab 1

--- first/src/controller/tab/Tab1Controller.java
+++ first/src/controller/tab/Tab1Controller.java
@@ -1,137 +1,100 @@
-/*
- * Created by wallflower on 9/1/2017.
-
-
 package controller.tab;
 
 import controller.MainController;
-import javafx.application.Application;
-import javafx.beans.value.ChangeListener;
-import javafx.beans.value.ObservableValue;
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.control.ListView;
+import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
 
 public class Tab1Controller {
 
     private MainController main;
-
-    @FXML
-    public Label lbl1;
-    @FXML
-    private TextField txt1;
-    @FXML
-    private Button btn1Save;
-    @FXML
-    private Button btn1Send;
-
-    @FXML
-    public void btn1SaveClicked(ActionEvent event) extends Application
-
-    {
-        System.out.println("Btn 1 save clicked");
-
-
-        lbl1.setText(txt1.getText());
-
-        TextField txt = new TextField();
-        txt.setPromptText("Search");
-        txt.textProperty().addListener(
-                new ChangeListener() {
-                    public void changed(ObservableValue observable, Object oldVal, Object newVal) {
-                        handleSearchByKey2((String) oldVal, (String) newVal);
-                    }
-                });
-
-
-    /*@FXML private void btn1SendClicked(ActionEvent event) {
-        System.out.println("Btn 1 send clicked");
-        // Send text from txt1 to lbl2 on Tab 2
-        main.setTab2LabelText(txt1.getText());
-    }
-
-    public void init(MainController mainController) {
-        main = mainController;
-    }
-
-
-
-
-    public void handleSearchByKey2(String oldVal, String newVal) {
-
-        ///////////////////////////////////////
-
-        ObservableList<String> entries = FXCollections.observableArrayList();
-        ListView list = new ListView();
-
-        //////////////////////////////
-        // If the number of characters in the text box is less than last time
-        // it must be because the user pressed delete
-        if (oldVal != null && (newVal.length() < oldVal.length())) {
-            // Restore the lists original set of entries
-            // and start from the beginning
-            list.setItems(entries);
-        }
-
-        // Break out all of the parts of the search text
-        // by splitting on white space
-        String[] parts = newVal.toUpperCase().split(" ");
-
-        // Filter out the entries that don't contain the entered text
-        ObservableList<String> subentries = FXCollections.observableArrayList();
-        for (Object entry : list.getItems()) {
-            boolean match = true;
-            String entryText = (String) entry;
-            for (String part : parts) {
-                // The entry needs to contain all portions of the
-                // search string *but* in any order
-                if (!entryText.toUpperCase().contains(part)) {
-                    match = false;
-                    break;
-                }
-            }
-
-            if (match) {
-                subentries.add(entryText);
-            }
-        }
-        list.setItems(subentries);
-    }
-}
-}
-*/
-
-package controller.tab;
-
-import javafx.event.ActionEvent;
-import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
-import controller.MainController;
-
-public class Tab1Controller {
-
-    private MainController main;
+    static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
+    private static final String url = "jdbc:mysql://localhost:3306/my_db";
+    private static final String user = "root";
+    private static final String password = "4ndp5$$$";
 
     @FXML public Label lbl1;
     @FXML private TextField txt1;
     @FXML private Button btn1Save;
     @FXML private Button btn1Send;
+    private PreparedStatement preparedStatement;
+    private String query = " ";
+    private TextArea ta = new TextArea();
 
     @FXML private void btn1SaveClicked(ActionEvent event) {
         System.out.println("Btn 1 save clicked");
 
         lbl1.setText(txt1.getText());
+
+
+        try {
+            // Register JDBC driver
+            Class.forName("com.mysql.jdbc.Driver");
+            System.out.println("Driver Laoded");
+
+            //Open a connection
+            Connection conn = DriverManager.getConnection(url, user, password);
+            System.out.println("Success Connection with the database");
+
+            // Execute a query
+            /*System.out.println("Creating statement...");
+            Statement stmt = conn.createStatement();
+            String sql;
+            sql = "SELECT * FROM Measurements";
+            ResultSet rs = stmt.executeQuery(sql);
+            */
+            //String queryString = "select" + query + "where" + where.getText ();
+            //ResultSet rs = stmt.executeQuery(sql);
+            System.out.println("Creating statement...");
+            String queryString = "SELECT * FROM Measurements  WHERE Confirmed = ?  " ;
+            preparedStatement = conn.prepareStatement(queryString);
+            preparedStatement.setString(1,txt1.getText());
+            ResultSet rs = preparedStatement.executeQuery();
+
+            /*String queryString = "select" + query + "where" + txt1.getText ();
+            preparedStatement = conn.prepareStatement (queryString);
+            preparedStatement.setString(1,txt1.getText());
+            ResultSet rs = preparedStatement.executeQuery ( );*/
+
+            while (rs.next()) {
+
+                //System.out.println("mpikeeeee");
+
+                //Retrieve by column name
+                String terminalname = rs.getString("TerminalName");
+                String location = rs.getString("Location");
+                String type_calculation = rs.getString("Type_Calc");
+                String date_time = rs.getString("Date_Time");
+                String confirmation = rs.getString("Confirmed");
+
+
+                //String finalstring = filter(terminalname) + filter(location) + filter(type_calculation) + filter(date_time) + filter(confirmation);
+                //ta.setText(finalstring);
+
+                //Display values
+                System.out.format("%s, %s, %s, %s, %s\n", terminalname, location, type_calculation, date_time, confirmation);
+                /*System.out.println("TerminalName: " + terminalname);
+                System.out.println("Location: " + location);
+                System.out.println("Type & Calculation: " + type_calculation);
+                System.out.println("Date & Time: " + date_time);
+                System.out.println("Confirmed: " + confirmation);*/
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+
     }
-
-
-
     public void init(MainController mainController) {
         main = mainController;
     }
